b2bafcab6a55ec01735c15fd0b0065ed0052c7ba
LeiHolmes <xulei964270141@qq.com>
2017-05-31 14:37:04 +0800

    添加注释

--- app/src/main/java/com/holmeslei/mvpdemo/view/LoginActivity.java
+++ app/src/main/java/com/holmeslei/mvpdemo/view/LoginActivity.java
@@ -11,11 +11,28 @@ import com.holmeslei.mvpdemo.R;
 import com.holmeslei.mvpdemo.presenter.LoginPresenter;
 import com.holmeslei.mvpdemo.presenter.LoginPresenterImpl;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+/**
+ * Description:   1. 使用MVP模式之后，Activity主要做一些UI上的操作。
+ *                2. LoginView封装一些操作UI的方法，由LoginActivity实现。
+ *                3. LoginPresenter负责View与Model的交互，由LoginPresenterImpl实现。
+ *                   LoginPresenter接收到View的登录点击事件后，将业务逻辑交给LoginModel去处理。
+ *                4. LoginModel负责具体的业务逻辑处理，由LoginModelImple实现
+ *                   这里是判断输入的用户名与密码是否正确，将处理结果返回给LoginPresenter。
+ *                5. LoginPresenter根据返回的不同数据通过LoginView调用View上不同的操作
+ * author         xulei
+ * Date           2017/5/31 11:58
+ */
 public class LoginActivity extends Activity implements LoginView {
-    private EditText etName;
-    private EditText etPassword;
-    private ProgressBar progressBar;
+    @BindView(R.id.login_progress)
+    ProgressBar progressBar;
+    @BindView(R.id.login_name)
+    EditText etName;
+    @BindView(R.id.login_password)
+    EditText etPassword;
 
     private LoginPresenter loginPresenter;
 
@@ -23,16 +40,11 @@ public class LoginActivity extends Activity implements LoginView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        initView();
+        ButterKnife.bind(this);
         loginPresenter = new LoginPresenterImpl(this);
     }
 
-    private void initView() {
-        etName = (EditText) findViewById(R.id.login_name);
-        etPassword = (EditText) findViewById(R.id.login_password);
-        progressBar = (ProgressBar) findViewById(R.id.login_progress);
-    }
-
+    @OnClick(R.id.login_start)
     public void onLoginClick(View view) {
         loginPresenter.comfirmLogin(etName.getText().toString(), etPassword.getText().toString());
     }
